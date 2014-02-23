7a62349d0eb62f816186c7e419ef422b65ddba4f
Bozhin Zafirov <bozhin@sirius.deck17.com>
2014-02-23 22:24:10 +0200

    gssh initial import

--- README.md
+++ README.md
@@ -1,4 +1,33 @@
 gssh
-====
+----
 
 Run ssh command on a group of servers simultaneously.
+
+
+Requirements
+------------
+
+In order to use this gssh, the ssh binary from openssh package must be installed in user's path.
+
+
+Usage
+-----
+
+To build gssh use the following command:
+
+	go build gssh.go
+	./gssh -h
+
+
+A list of servers is mandatory to use gssh. The list is a plain text file with one server at a line (no username):
+
+	cat << EOF > servers.txt
+	server1.domain.tld
+	server2.domain2.tld
+	1.2.3.4
+	EOF
+
+
+To actually run a command on all files from the list:
+
+	./gssh -file servers.txt "uptime"
---
+++ gssh.go
@@ -0,0 +1,207 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+	"bufio"
+	"log"
+	"flag"
+	"fmt"
+	"io"
+	"sync"
+	"time"
+)
+
+
+/* ssh client group */
+type SshGroup struct {
+	mu      sync.Mutex
+	active  int
+}
+
+
+/* wait until there are at most "n" (or none) processes left */
+func (s *SshGroup) Wait(n int) {
+	var active int
+	for {
+		s.mu.Lock()
+		active = s.active
+		s.mu.Unlock()
+
+		if active == 0 || active < n {
+			break
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
+}
+
+
+/* print stdout/stderr with color */
+func PrintOutput(Std *bufio.Reader, Addr string, Padding int, Color int) {
+	for {
+		line, err := Std.ReadString('\n')
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("%*s%s %s->\033[0m %s",
+			Padding,
+			" ",
+			Addr,
+			fmt.Sprintf("\033[01;%dm", Color),
+			line)
+	}
+}
+
+
+/* connect to remote server */
+func (s *SshGroup) Command(Username, Address string, AddrPadding int, Command string) {
+	defer func() {
+		s.mu.Lock()
+		s.active--
+		s.mu.Unlock()
+	}()
+
+	cmd := exec.Command("env",
+		"ssh",
+		"-A",
+		"-o", "PasswordAuthentication=no",
+		"-o", "StrictHostKeyChecking=no",
+		"-o", "NumberOfPasswordPrompts=1",
+		"-o", "GSSAPIAuthentication=no",
+		"-o", "HostbasedAuthentication=no",
+		"-l", Username,
+		Address,
+		Command)
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	/* padding length */
+	Padding := AddrPadding - len(Address)
+	Stdout := bufio.NewReader(stdout)
+	Stderr := bufio.NewReader(stderr)
+
+	/* run the command */
+	cmd.Start()
+
+	var w sync.WaitGroup
+	w.Add(2)
+
+	go func() {
+		PrintOutput(Stdout, Address, Padding, 32)
+		w.Done()
+	}()
+
+	go func() {
+		PrintOutput(Stderr, Address, Padding, 31)
+		w.Done()
+	}()
+
+	w.Wait()
+
+}
+
+
+/* load servers list from a file */
+func LoadServerList(File string) (AddrPadding int, ServerList []string) {
+	file, err := os.Open(File)
+	if err != nil {
+		log.Fatal(err)
+	}
+	Reader := bufio.NewReader(file)
+	for Line, err := Reader.ReadString('\n'); err != io.EOF; Line, err = Reader.ReadString('\n') {
+		if AddrPadding < len(Line) {
+			AddrPadding = len(Line)
+		}
+		ServerList = append(ServerList, Line[:len(Line)-1])
+	}
+	return
+}
+
+
+/* global variables */
+var fCommand string
+var fUser string
+var fDelay int
+var fProcs int
+var fFile string
+//var fOutDir string
+//var fMacro string
+
+
+/* initialize */
+func init() {
+	/* commandline arguments */
+	flag.StringVar(&fUser, "user", "root", "ssh login as this username")
+	flag.StringVar(&fFile, "file", "", "file with the list of hosts")
+	flag.IntVar(&fDelay, "delay", 10, "delay between each ssh fork (default 10 msec)")
+	flag.IntVar(&fProcs, "procs", 500, "number of parallel ssh processes (default: 500)")
+	//flag.StringVar(&fOutDir, "outdir", "", "save the remote output in this directory")
+	//flag.StringVar(&fMacro, "macro", "", "run pre-defined commands macro")
+}
+
+
+/* main program */
+func main() {
+	/* parse commandline argiments */
+	flag.Parse()
+	if flag.NArg() < 1 {
+		log.Fatal("Missing command.")
+	}
+
+	/* sanity checks */
+	if fFile == "" {
+		log.Fatal("No serverlist file.")
+	}
+
+	fCommand = flag.Args()[0]
+	/* read server names from file */
+	AddrPadding, ServerList := LoadServerList(fFile)
+	fmt.Println(ServerList[0])
+
+	/* no point to display more processes than  */
+	if fProcs > len(ServerList) {
+		fProcs = len(ServerList)
+	}
+
+	/* print heading text */
+	fmt.Println("gssh - group ssh, ver. 0.1")
+	fmt.Println("(c)2014 Bozhin Zafirov <bozhin@deck17.com>")
+	fmt.Println()
+	fmt.Printf("  [*] read (%d) hosts from the list\n", len(ServerList))
+	fmt.Printf("  [*] executing '%s' as user '%s'\n", fCommand, fUser)
+	fmt.Printf("  [*] spawning %d parallel ssh sessions\n\n", fProcs)
+
+	/* make new group and spawn ssh processes */
+	ssh := &SshGroup{}
+	for _, Server := range ServerList {
+		/* run command */
+		ssh.mu.Lock()
+		ssh.active++
+		ssh.mu.Unlock()
+		go ssh.Command(
+			fUser,
+			Server,
+			AddrPadding,
+			fCommand)
+		/* time delay and max procs wait between spawn */
+		time.Sleep(time.Duration(fDelay) * time.Millisecond)
+		ssh.Wait(fProcs)
+	}
+	/* wait for ssh processes to exit */
+	ssh.Wait(0)
+
+	fmt.Println()
+	fmt.Printf("  Done. %d hosts processed.\n", len(ServerList))
+}
